37b62b60cb38866216cc7b0481a40a32a611a99c
Marcus <gemmyboy@gmail.com>
2016-07-01 13:35:25 -0500

    Release Build 1.0v

---
+++ app/src/main/java/gemmyboy/flashdeed/HomeScreen.java
@@ -0,0 +1,149 @@
+package gemmyboy.flashdeed;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class HomeScreen extends AppCompatActivity {
+
+    private DrawerLayout mNav;
+    private ListView mLV;
+    private String[] mOptions;
+    private ActionBarDrawerToggle mDT;
+
+
+    //----------------------------------------------------------------------------------------------
+    //Standard Life Cycle Functions that are called depending on where the flow
+    //  of the app is.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        //Setting-up the Toolbar -- Using Toolbar because it's cross-API compatible on devices
+        final Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_home);
+        myToolbar.setTitle("");
+        setSupportActionBar(myToolbar);
+
+        //Setting-up and launching the Nav-Drawer menu
+        mNav = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mLV = (ListView) findViewById(R.id.left_drawer);
+        mOptions = getResources().getStringArray(R.array.left_nav_menu);
+
+        mLV.setAdapter(new ArrayAdapter<String>(this, R.layout.nav_item, mOptions));
+        mLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Derp
+            }
+        });
+
+
+
+        mDT = new ActionBarDrawerToggle(this, mNav, myToolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mNav.setDrawerListener(mDT);
+    }//End onCreate()
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }//End onRestart()
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }//End onResume()
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }//End onPause()
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }//End onStop()
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }//End onDestroy()
+
+    //Called when initially creating the Toolbar Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.menu_home, m);
+        return true;
+    }//End onCreateOptionsMenu
+
+    //When an item is selected from the top-left menu on the Activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_settings:
+                Toast.makeText(HomeScreen.this, "Settings Dialog: Not Implemented", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_logout:
+                Intent in = new Intent(getApplicationContext(), LoginScreen.class);
+                startActivity(in);
+                finish();
+                return true;
+            default:
+                Toast.makeText(HomeScreen.this, item.getTitle() + ": Not Implemented", Toast.LENGTH_SHORT);
+                return true;
+        }//End Switch
+    }//End onOptionsItemSelected
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }//End onPrepareOptionsMenu()
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDT.syncState();
+    }//End onPostCreate()
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDT.onConfigurationChanged(newConfig);
+    }//End onConfigurationChanged()
+
+    //OnClick for menu item
+    public void OnClick_NavMenu(View v){
+        Toast.makeText(HomeScreen.this, ((Button)v).getText() + ": Not Implemented", Toast.LENGTH_SHORT).show();
+    }//End OnClick_NavMenu()
+
+
+}//End HomeScreen class
--- app/src/main/java/gemmyboy/flashdeed/LoginScreen.java
+++ app/src/main/java/gemmyboy/flashdeed/LoginScreen.java
@@ -1,7 +1,13 @@
 package gemmyboy.flashdeed;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -39,4 +45,35 @@ public class LoginScreen extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
     }//End onDestroy()
+
+    //----------------------------------------------------------------------------------------------
+
+    //Onclick function for Login button
+    public void OnClick_Login(View v) {
+        String username = ((EditText)findViewById(R.id.text_field_username)).getText().toString();
+        String password = ((EditText)findViewById(R.id.text_field_password)).getText().toString();
+
+        //Null error check
+        if (username == null || password == null) {
+            Toast.makeText(getApplicationContext(), "Please enter your Username && Password", Toast.LENGTH_SHORT).show();
+            return;
+        }//End if
+
+        Log.d("Derp", "Username: " + username.toLowerCase() + " | Password: " + password.toLowerCase());
+
+        String admin = "admin";
+
+        //Basic login cred until backend system and DB are setup.
+        if (username.equalsIgnoreCase(admin) && password.equalsIgnoreCase(admin)) {
+            Intent in = new Intent(this, HomeScreen.class);
+            startActivity(in);
+        } else {
+            Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+        }//End else
+    }//End OnClick_Login()
+
+    //Onclick function for Register button
+    public void OnClick_Register(View v) {
+        Toast.makeText(getApplicationContext(), "Register Feature: Not Implemented", Toast.LENGTH_SHORT).show();
+    }//End OnClick_Login()
 }//End LoginScreen class
